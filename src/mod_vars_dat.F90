a43a0fa6bcc0d0c860de6b640b86aec79f89af60
Agustin Garcia <agustin@atmosfera.unam.mx>
2021-03-22 23:40:47 -0600

    no common blocks

--- src/mod_vars_dat.F90
+++ src/mod_vars_dat.F90
@@ -137,11 +137,7 @@ logical,allocatable :: tvar(:)
 !> true if input emissions files contains density population
 logical :: tpob
 ! Domain Variables
-common /domain/ L_CO,NDIMS,nvars,zlev,dix,djx,eix,ejx,dx,dy,dxe,dye,rama,Title
-common /date/ id_grid,unlimdimid,Times,current_date,cday,mecha,tpob
-common /wrf/ julyr,julday,mapproj,iswater,islake,isice,isurban,isoilwater,&
-            cenlat,cenlon,trulat1, trulat2,moadcenlat,stdlon,pollat,pollon,&
-            gmt,mminlu
+
 contains
 !>   @brief Compara los valores para los maximos
 !>   @author  Jose Agustin Garcia Reynoso
